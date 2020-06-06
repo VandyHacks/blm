b2f43f41417c046c7e9739b53ef02fc0c0530c46
Gabriel Ting <gabrielfting@gmail.com>
2020-06-06 18:18:10 -0500

    Add fonts and customize to VH

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Educate. Donate. Act.</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -16,7 +16,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: white;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
