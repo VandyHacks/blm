caf63ad278c9cd4127a54de6a7928ab8b7c071f0
Gabriel Ting <gabrielfting@gmail.com>
2020-06-07 18:55:42 -0500

    Finish charity list

--- src/App.svelte
+++ src/App.svelte
@@ -20,26 +20,26 @@
 	Through self-education, we seek to better understand systemic racism. Some of the resources we are using are:
 	<ul>
 		<li>
-			<a href="https://blacklivesmatters.carrd.co/#educate">
+			<a href="https://blacklivesmatters.carrd.co/#educate" target="_blank">
 				Collated resources on Black Lives Matter and anti-racism
 			</a>
 		</li>
 		<li>
-			<a href="https://www.fastcompany.com/40528096/8-ways-to-make-real-progress-on-techs-diversity-problem">
+			<a href="https://www.fastcompany.com/40528096/8-ways-to-make-real-progress-on-techs-diversity-problem" target="_blank">
 				8 Ways to Make Real Progress on Tech's Diversity Problem</a> 
 			</li>
 		<li>
-			<a href="https://medium.com/@marksluckie/what-it-s-actually-like-to-be-a-black-employee-at-a-tech-company-e32bb222818b">
+			<a href="https://medium.com/@marksluckie/what-it-s-actually-like-to-be-a-black-employee-at-a-tech-company-e32bb222818b" target="_blank">
 				A view into being a Black employee at a tech company
 			</a>
 		</li>
 		<li>
-			<a href="https://www.brookings.edu/research/black-and-hispanic-underrepresentation-in-tech-its-time-to-change-the-equation/">
+			<a href="https://www.brookings.edu/research/black-and-hispanic-underrepresentation-in-tech-its-time-to-change-the-equation/" target="_blank">
 				Report on Black and Hispanic underrepresentation in tech
 			</a>
 		</li>
 	</ul>
-	
+
 	<hr>
 	Through listening, we seek to learn from the experiences of members of our community. Please let us know your thoughts on how we can best support the community, and use what platform we have to spread resources and information. We want to continue this conversation and welcome any input on how we can make ourselves better and positively impact our current and future workspaces.
 
@@ -48,26 +48,87 @@
 
 	<h2>Donate.</h2>
 	<hr>
-	We are matching donations up to a total of $1,000 to any charity fighting systemic injustice. We have attached a list of charities that we chose as recommendations to you. As a result of restrictions on student organization accounts at Vanderbilt, we will be donating all the matches to the NAACP Legal Defense Fund. We highly encourage anyone who has the means to donate what they can. Please send us the receipt so that VandyHacks can match all of your donations. 
+	We are matching donations up to a total of $1,000 to any organization fighting racism and anti-Blackness. As a result of restrictions on student organization accounts at Vanderbilt, VandyHacks will be donating all the matches to the NAACP Legal Defense Fund. We highly encourage anyone who has the means to donate what they can. Please send us the receipt at <a href="mailto:info@vandyhacks.org">info@vandyhacks.org</a> so that VandyHacks can match all of your donations. 
 
 	<br>
 	<br>
 
-	Some of the organizations we are donating to include:
+	Some of the organizations our members individually are donating to include:
 
 	<div class="fund">
 		<h3>NAACP Legal Defense Fund</h3>
 		<div class="summary">
-			The NAACP Legal Defense Fund is a non-profit organization that 
+			The NAACP Legal Defense Fund is a non-profit organization that seeks "structural changes to expand democracy, eliminate disparities, and achieve racial justice in a society that fulfills the promise of equality for all Americans."
 		</div> 
-		<button>Donate to the NAACP LDF</button>
+		<a class="button button-primary" href="https://www.naacpldf.org/" target="_blank">Donate to the NAACP LDF</a>
 	</div>
+
+	<div class="fund">
+		<h3>Campaign Zero</h3>
+		<div class="summary">
+			Non-profit seeking data-driven policy solutions from local, state, and federal lawmakers to take immediate action to end police violence.
+		</div> 
+		<a class="button button-primary" href="https://www.joincampaignzero.org/" target="_blank">Donate to Campaign Zero</a>
+	</div>
+
+	<div class="fund">
+		<h3>Thurgood Marshall College Fund</h3>
+		<div class="summary">
+			Non-profit that supports and provides opportunities for the Black College Community "through scholarships, capacity builder and research initiatives, innovative programs, and strategic partnerships."
+		</div> 
+		<a class="button button-primary" href="https://www.tmcf.org" target="_blank">Donate to the TMCF</a>
+	</div>
+
+	<div class="fund">
+		<h3>Code 2040</h3>
+		<div class="summary">
+			Non-profit "activating, connecting, and mobilizing the largest racial equity community in tech to dismantle the structural barriers that prevent the full participation and leadership of Black and Latinx technologists in the innovation economy."
+		</div> 
+		<a class="button button-primary" href="http://www.code2040.org/" target="_blank">Donate to Code 2040</a>
+	</div>
+
+	<div class="fund">
+		<h3>My Brother's Keeper Alliance</h3>
+		<div class="summary">
+			Focused on building safe and supportive communities for boys and young men of color where they feel valued and have clear pathways to opportunity and success.
+		</div> 
+		<a class="button button-primary" href="https://www.obama.org/mbka/" target="_blank">Donate to MBKA</a>
+	</div>
+
+	<div class="fund">
+		<h3>Black Girls Code</h3>
+		<div class="summary">
+			Providing coding lessons to young girls from underrepresented communities by reaching out to the community through workshops and after school programs. Black Girls CODE's ultimate goal is to train 1 million girls by 2040.
+		</div> 
+		<a class="button button-primary" href="https://www.blackgirlscode.com" target="_blank">Donate to Black Girls Code</a>
+	</div>
+
+	<div class="fund">
+		<h3>Official George Floyd Fund</h3>
+		<div class="summary">
+			Established to cover funeral and burial expenses, mental and grief counseling, lodging and travel for all court proceedings, and to assist the Floyd family in the days to come as they continue to seek justice for George.
+		</div> 
+		<a class="button button-primary" href="https://www.gofundme.com/f/georgefloyd" target="_blank">Donate to the George Floyd Fund</a>
+	</div>
+
+	<div class="fund">
+		<h3>NAACP</h3>
+		<div class="summary">
+			The mission of the National Association for the Advancement of Colored People (NAACP) is to secure the political, educational, social, and economic equality of rights in order to eliminate race-based discrimination and ensure the health and well-being of all persons.
+		</div> 
+		<a class="button button-primary" href="https://www.naacp.org" target="_blank">Donate to the NAACP</a>
+	</div>
+
 	<br>
 	<br>
 	
 	<h2>Act.</h2>
 	<hr>
 	There are many ways that you and I can be allies. Taking action by calling and writing letters to your representatives, participating in peaceful demonstrations, and signing petitions are ways that we can contribute. 
+	
+	<br>
+	<br>
+	- The VandyHacks Team
 </div>
 </main>
 
@@ -90,11 +151,11 @@
 	}
 
 	.fund {
-		margin: 20px 0 0 0;
+		margin: 2em 0 0 0;
 	}
-	
-	button {
-		color: white;
+
+	.button-primary {
+		margin: .5em 0 0 0;
 	}
 
 	@media (min-width: 640px) {
@@ -109,6 +170,10 @@
 
 		h2 {
 			font-size: 2em;
+		}
+
+		.button-primary {
+			font-size: .75em;
 		}
 	}
 </style>
