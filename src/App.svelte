678e81e1e25a168be434c8ff3e6431c54e0360d8
Gabriel Ting <gabrielfting@gmail.com>
2020-06-06 23:14:08 -0500

    Basic styling and skeleton.css

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,59 @@
-<script>
-	export let name;
-</script>
-
 <main>
 	<h1>Educate. Donate. Act.</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="content">
+	We at VandyHacks condemn the systemic racism and oppression gripping our society that senselessly murdered George Floyd, Breonna Taylor, Ahmaud Abery, and countless other Black people. These actions have shocked and saddened us, and we denounce unjust deaths at the hands of institutional racism.
+
+	<br>
+	<br>
+	We believe that we must act against these forces, in whatever way that we can. While strange to speak as a hackathon community, we believe we must work to support the organizations and people doing critical work. We believe in equal access to education, resources, and opportunities, especially in tech, and are taking action. 
+	<br>
+	<br>
+	
+	<hr>
+	Our action comes in the form of self-education, listening, and donations.
+	<br>
+	<br>
+
+	Through self-education, we seek to better understand systemic racism. Some of the resources we are using are:
+	<ul>
+		<li>
+			<a href="https://blacklivesmatters.carrd.co/#educate">
+				Collated resources on Black Lives Matter and anti-racism
+			</a>
+		</li>
+		<li>
+			<a href="https://www.fastcompany.com/40528096/8-ways-to-make-real-progress-on-techs-diversity-problem">
+				8 Ways to Make Real Progress on Tech's Diversity Problem</a> 
+			</li>
+		<li>
+			<a href="https://medium.com/@marksluckie/what-it-s-actually-like-to-be-a-black-employee-at-a-tech-company-e32bb222818b">
+				A view into being a Black employee at a tech company
+			</a>
+		</li>
+		<li>
+			<a href="https://www.brookings.edu/research/black-and-hispanic-underrepresentation-in-tech-its-time-to-change-the-equation/">
+				Report on Black and Hispanic underrepresentation in tech
+			</a>
+		</li>
+	</ul>
+	<hr>
+	Through listening, we seek to learn from the experiences of members of our community. Please let us know your thoughts on how we can best support the community, and use what platform we have to spread resources and information. We want to continue this conversation and welcome any input on how we can make ourselves better and positively impact our current and future workspaces.
+
+	<br>
+	<br>
+	<hr>
+	We are matching donations up to a total of $1,000 to any charity fighting systemic injustice. We have attached a list of charities that we chose as recommendations to you. As a result of restrictions on student organization accounts at Vanderbilt, we will be donating all the matches to the NAACP Legal Defense Fund. We highly encourage anyone who has the means to donate what they can. Please send us the receipt so that VandyHacks can match all of your donations. 
+
+	<br>
+	<br>
+	<hr>
+	There are many ways that you and I can be allies. Taking action by calling and writing letters to your representatives, participating in peaceful demonstrations, and signing petitions are ways that we can contribute. 
+	</div>
 </main>
 
 <style>
 	main {
+		color: white;
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -18,13 +63,21 @@
 	h1 {
 		color: white;
 		text-transform: uppercase;
-		font-size: 4em;
 		font-weight: 100;
 	}
 
+	/* #content {
+		text-align: left;
+	} */
+
 	@media (min-width: 640px) {
 		main {
-			max-width: none;
+			max-width: 60%;
+			font-size: 1.5em;
+		}
+		h1 { 
+			font-size: 3em;
+			font-weight: 100;
 		}
 	}
 </style>
